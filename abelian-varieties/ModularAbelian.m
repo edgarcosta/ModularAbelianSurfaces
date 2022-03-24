06aaf9ac1b989d81b7526b40eae5efef1653a2b3
Edgar Costa <edgarc@mit.edu>
2022-03-24 21:52:42 +0000

    dolabel

--- abelian-varieties/ModularAbelian.m
+++ abelian-varieties/ModularAbelian.m
@@ -1,4 +1,4 @@
-
+declare verbose ModAbVarRec, 3;
 
 intrinsic GetDimension2Factors(N) -> SeqEnum
 { FIXME }
@@ -18,15 +18,20 @@ intrinsic NormalizedPeriods(P::ModMatFldElt : prec:=80) -> ModMatFldElt
     return P;
 end intrinsic;
 
+// use hecke to get the number of correct digits
 intrinsic PeriodsFromModSymb(f::ModSym : prec:=80, ncoeffs:=10000) -> ModMatFldElt
 { FIXME }
+    vprint ModAbVarRec: Sprintf("Computing periods, prec:=%o, ncoeffs:=%o", prec, ncoeffs);
+    vprintf ModAbVarRec: Sprintf("%o...", f);
     default_prec := Precision(GetDefaultRealField());
     // this is how we control the precision of the output of Periods
     SetDefaultRealFieldPrecision(prec + 10);
+    vtime ModAbVarRec:
     pi_f := PeriodMapping(f, ncoeffs);
     P:= [pi_f(b) : b in Basis(f)];
     // undo the default_prec
     SetDefaultRealFieldPrecision(default_prec);
+    vprint ModAbVarRec: "Done";
     return NormalizedPeriods(Matrix(P) : prec:=prec);
 end intrinsic;
 
@@ -40,10 +45,17 @@ intrinsic PeriodsWithMaximalOrder(P::ModMatFldElt) -> ModMatFldElt, SeqEnum
     such that the coefficient ring index is > 1, return a period matrix for an isogenous abelian variety
     such that the isogenous abelian variety has endomorphism ring equal to the maximal order
 }
+    vprintf ModAbVarRec: "Extending lattice...";
     QQ := RationalsExtra(Precision(BaseRing(P)));
+    vprint ModAbVarRec: "\nComputing GeometricEndomorphismRepresentation...";
+    vtime ModAbVarRec:
     GeoEndoRep := GeometricEndomorphismRepresentation(P, QQ);
     F, h := InclusionOfBaseExtra(BaseRing(GeoEndoRep[1][1]));
     GeoEndoRepBase := EndomorphismRepresentation(GeoEndoRep, F, h);
+    vprint ModAbVarRec:GeoEndoRep;
+    vprint ModAbVarRec:GeoEndoRepBase;
+    vprint ModAbVarRec: "Done";
+    require #GeoEndoRepBase eq 2: Sprintf("This does not seem to be GL2-type, dim End A = %o", #GeoEndoRepBase);
     one := GeoEndoRepBase[1][2];
     gen := GeoEndoRepBase[2][2];
     assert one eq 1;
@@ -62,28 +74,71 @@ intrinsic PeriodsWithMaximalOrder(P::ModMatFldElt) -> ModMatFldElt, SeqEnum
     assert bool;
     S, P, Q := SmithForm(Matrix(Integers(), kernel));
     P2 := Submatrix(AuxP*Matrix(CC, P^-1), 1, 5, 2, 4);
+    vprintf ModAbVarRec: "Computing GeometricEndomorphismRepresentation...";
+    vtime ModAbVarRec:
     GeoEndoRep2 := GeometricEndomorphismRepresentation(P2, QQ);
+    vprint ModAbVarRec: "Done";
+    require #GeoEndoRep2 ge 2: Sprintf("This does not seem to be GL2-type, dim End newA_bar = %o", #GeoEndoRep2);
+    F, h := InclusionOfBaseExtra(BaseRing(GeoEndoRep2[1][1]));
     GeoEndoRepBase2 := EndomorphismRepresentation(GeoEndoRep2, F, h);
+    vprint ModAbVarRec:GeoEndoRep2;
+    vprint ModAbVarRec:GeoEndoRepBase2;
+    require #GeoEndoRepBase2 ge 2: Sprintf("This does not seem to be GL2-type, dim End newA = %o", #GeoEndoRepBase2);
     //comp := MinimalPolynomial(GeoEndoRepBase2[2][1]);
     //exp := MinimalPolynomial(Integers(K).2);
     //exp2 := MinimalPolynomial(-Integers(K).2);
     //require comp in {exp, exp2} : Sprintf("%o \noin {%o, %o}", comp, exp, exp2);
+    vprint ModAbVarRec: "Done";
     return P2, GeoEndoRepBase2;
 end intrinsic;
 
 
-intrinsic ReconstructGenus2Curve(P::ModMatFldElt) -> ModMatFldElt, SeqEnum
+intrinsic ReconstructGenus2Curve(P::ModMatFldElt : tries:=10) -> ModMatFldElt, SeqEnum
 { FIXME }
-    bool, pol := SomePrincipalPolarization(P);
-    assert bool;
-    CC := BaseRing(P);
-    QQ := RationalsExtra(Precision(CC));
-    E, F := FrobeniusFormAlternating(Matrix(Integers(), pol));
-    H := ReconstructCurve(P*Transpose(ChangeRing(F, CC)), QQ);
-    H := ReducedMinimalWeierstrassModel(H);
-    return H;
+    for i in [1..tries] do
+        vprintf ModAbVarRec: "Finding a polarization...";
+        vtime ModAbVarRec:
+        bool, pol := SomePrincipalPolarization(P);
+        vprint ModAbVarRec: "Done";
+        assert bool;
+        CC := BaseRing(P);
+        QQ := RationalsExtra(Precision(CC));
+        E, F := FrobeniusFormAlternating(Matrix(Integers(), pol));
+        vprintf ModAbVarRec: "Reconstructing curve...";
+        vtime ModAbVarRec:
+        C := ReconstructCurve(P*Transpose(ChangeRing(F, CC)), QQ);
+        vprint ModAbVarRec: "Done";
+        if BaseField(C) cmpeq Rationals() then
+            return true, C;
+        else
+            vprint ModAbVarRec: "Not over QQ";
+        end if;
+    end for;
+    return false;
 end intrinsic;
 
+
+
+intrinsic ReconstructGenus2Curve(f::ModSym : prec:=80, ncoeffs:=10000) -> ModMatFldElt, SeqEnum
+{
+    FIXME
+}
+    P := PeriodsFromModSymb(f : prec:=prec, ncoeffs:=ncoeffs);
+    P2, G2 := PeriodsWithMaximalOrder(P);
+    return ReconstructGenus2Curve(P2);
+end intrinsic;
+
+
+intrinsic MakeNewformModSym(level::RngIntElt, hecke_cutters::SeqEnum[Tup]) -> ModSym
+{ FIXME }
+    R<x> := PolynomialRing(Rationals());
+    // SetVerbose("ModularSymbols", true);
+    chi := DirichletGroup(level)!1;
+    hecke_cutters := [<elt[1], R!elt[2]> : elt in hecke_cutters];
+    Snew := NewSubspace(CuspidalSubspace(ModularSymbols(chi,2)));
+    f := Kernel(hecke_cutters ,Snew);
+    return f;
+end intrinsic;
 
 
 
---
+++ abelian-varieties/dolabel.m
@@ -0,0 +1,18 @@
+AttachSpec("~/projects/CHIMP/CHIMP.spec");
+AttachSpec("~/projects/ModularCurves/abelian-varieties/spec");
+s := Split(input, ":");
+label := s[1];
+level := StringToInteger(s[2]);
+hc := eval s[3];
+prec := StringToInteger(prec);
+ncoeffs := Ceiling(20*Sqrt(level)*Log(10)*prec/(2*Pi(ComplexField())));
+f := MakeNewformModSym(level, hc);
+try
+  b, H := ReconstructGenus2Curve(f : prec:=prec, ncoeffs:=ncoeffs);
+  assert b;
+  f, g := HyperellipticPolynomials(MinimalWeierstrassModel(H));
+catch e
+  exit 1;
+end try;
+StripWhiteSpace(Sprintf("%o:[%o,%o]", label, Eltseq(f), Eltseq(g)));
+exit;
