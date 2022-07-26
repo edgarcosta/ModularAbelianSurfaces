0782ad3fbd04c466721a5ad4e3359d3af65750a9
Edgar Costa <edgarc@mit.edu>
2022-07-26 15:50:08 +0000

    new defaults

--- abelian-varieties/ModularAbelian.m
+++ abelian-varieties/ModularAbelian.m
@@ -393,7 +393,7 @@ end intrinsic;
 
 
 // FIXME, the output might not be a curve, but just igusa invariants over some numberfield
-intrinsic ReconstructGenus2Curve(f::ModSym : prec:=80, ncoeffs:=10000, D:=[-3..3], UpperBound:=14) -> BoolElt, Any
+intrinsic ReconstructGenus2Curve(f::ModSym : prec:=80, ncoeffs:=10000, D:=[-4..4], UpperBound:=12) -> BoolElt, Any
 {
 TODO: add documentation
 }
