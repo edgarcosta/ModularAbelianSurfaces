a61ed1e2a00a4ce09293686379d7fe870e15cd0f
Sachi Hashimoto <svh@bu.edu>
2023-04-27 10:51:15 +0200

    add intrinsic for quotient and subvariety homology, tests

--- abelian-varieties/ModularAbelian.m
+++ abelian-varieties/ModularAbelian.m
@@ -264,6 +264,68 @@ intrinsic PeriodMatrixWithMaximalOrder(P::ModMatFldElt) -> ModMatFldElt, SeqEnum
   return P2, GeoEndoRepBase2;
 end intrinsic;
 
+
+intrinsic NewformLattices(f::ModSym) -> ModMatRngElt, AlgMatElt, ModMatRngElt, AlgMatElt, AlgMatElt
+  {Find the homology for the abelian subvariety associated to f and the quotient variety of J0N, and the intersection pairing}
+    A := AmbientSpace(f);
+    CS := CuspidalSubspace(A); // we are assuming that this comes equipped with an integral basis, this is "H"
+
+    _, _, psi := VectorSpace(A);
+    fromCS := Matrix(Integers(), [Eltseq(psi(elt)) : elt in Basis(CuspidalSubspace(A))]);
+
+    p := 1;
+    desired_rank := Dimension(CS) - Dimension(f); // rank of If*H
+    H := ZeroMatrix(Integers(), Dimension(CS), 0);
+    V := ZeroMatrix(Integers(), 0, Dimension(CS));
+    // we are going this method as NewSubspace basis is not always equipped with an integral, e.g., 285.2.a.d
+    while Rank(H) ne desired_rank do
+        p := NextPrime(p);
+        while Level(f) mod p eq 0 do
+            p := NextPrime(p);
+        end while;
+        Tp := Matrix(Integers(), HeckeOperator(CS, p)); //this is 30 x 30
+        h := ChangeRing(MinimalPolynomial(HeckeOperator(f, p)), Integers()); //degree 2
+        hp := Evaluate(h, Tp);
+        H := HorizontalJoin(H, hp);
+        V := VerticalJoin(V, hp);
+    end while;
+    Hsub := KernelMatrix(H);//this is supposed to be H[If], and If = (hp)_p, dim 4
+    //If := Ker (TT -> Z[...an(f)...]) T_n -> an(f), and min poly of an(f) is h
+    D, P, Q := SmithForm(V);
+    if Nrows(D) gt 0 then
+      assert Diagonal(D)[Ncols(D)-Dimension(f)+1..Ncols(D)] eq [0 : _ in [1..Dimension(f)]];
+    end if;
+    
+    // this basis projects down to the standard basis of (H/If H)_free
+    // these are basis for Hquo
+    Hquo := Submatrix(Q^-1, Nrows(Q) - Dimension(f) + 1, 1, Dimension(f), Ncols(Q));
+
+    assert Rank(VerticalJoin(V, Hquo)) eq Dimension(CS);
+    
+    //this is the projection of H_sub in (H/If H)_free
+    Hsub_in_Hquo := Hsub*Submatrix(Q, 1, Ncols(Q) - Dimension(f) + 1, Nrows(Q), Dimension(f));
+    
+    //we now invert Hsub_in_Hquo not caring about rescaling
+    Hquo_in_Hsub := Matrix(Rationals(), Hsub_in_Hquo)^-1;
+    //Denominator(Hquo_in_Hsub);
+    Hquo_in_Hsub *:= Denominator(Hquo_in_Hsub);
+    Hquo_in_Hsub := Matrix(Integers(), Hquo_in_Hsub);
+    
+    S := Matrix(Integers(), KernelMatrix(VerticalJoin( Matrix(Rationals(), Hsub*fromCS), -Matrix([Eltseq(b) : b in Basis(f)]))));
+    assert Submatrix(S, 1,1, Dimension(f), Dimension(f)) eq 1;
+    // how to write Hsub basis in terms of Basis(f)
+    Hsub_in_Bf := Submatrix(S, 1,1 + Dimension(f), Dimension(f), Dimension(f));
+    
+    // how to write Hquo in terms of Basis(f)
+    Hquo_in_Bf := Hquo_in_Hsub*Hsub_in_Bf;
+    
+    Ef := IntersectionPairing(f);
+    Esub, Equo := Explode([Matrix(Integers(), A*Denominator(A)) where A := S*Ef*Transpose(S) where S:=Matrix(Rationals(), elt) : elt in [Hsub_in_Bf, Hquo_in_Bf]]);
+    
+    return Hsub, Esub, Hquo, Equo,Hsub_in_Bf;
+end intrinsic;
+
+
 /*
 intrinsic SomeIsogenousPrincipallyPolarized(P::ModMatFldElt: D:= [-10..10]) -> Assoc
 {
