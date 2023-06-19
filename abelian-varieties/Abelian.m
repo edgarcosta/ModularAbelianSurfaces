805669c1a196ea2662fd0d58fd618d0d2eb43d36
Edgar Costa <edgarc@mit.edu>
2023-06-19 12:21:12 -0400

    simpler code and documentation

--- abelian-varieties/Abelian.m
+++ abelian-varieties/Abelian.m
@@ -1,6 +1,8 @@
 
 intrinsic DualLattice(Omega::ModMatFldElt) -> ModMatFldElt
 {
+    Input: Omega, g by 2g complex matrix, representing a complex torus A
+    Output: Omega^v, g by 2g complex matrix, representing the dual torus A^v with the dual basis for the lattice.
 }
     CC<I> := BaseRing(Omega);
     g := Nrows(Omega);
@@ -11,7 +13,10 @@ intrinsic DualLattice(Omega::ModMatFldElt) -> ModMatFldElt
 end intrinsic;
 
 intrinsic DualLatticeWithRespectToPairing(Omega::ModMatFldElt, E::AlgMatElt) -> ModMatFldElt
-{ 
+{
+    Input: Omega g by 2g complex matrix arising from an abelian variety A/Q with a Q-basis of H^0(A,Omega^1), together with an (alternating) 2g by 2g integer matrix E representing a polarization A to A^v (defined over Q) with respect to the Z-bases of homology given by Omega and the Omega^v given by the DualLattice function.
+
+Output: Omega^v_Q, g by 2g complex matrix arising from the dual abelian variety A^v together with the Q-basis of H^0(A^v,\Omega^1) that pulls back under A to A^v to the specified Q-basis of H^0(A,\Omega^1)
 }
     CC<I> := BaseRing(Omega);
     OmegaDualCC := DualLattice(Omega);
@@ -22,6 +27,9 @@ end intrinsic;
 
 intrinsic SelfDualHomomorphismsg(Omega::ModMatFldElt, E::AlgMatElt) -> SeqEnum, Map
 {
+    Input: Omega, 2g by 2g complex matrix representing a complex torus A, together with an (alternating) 2g by 2g integer matrix E irepresenting a polarization A to A^v (defined over Q)
+
+    Output: A Z-basis of self-dual homomorphisms from A to A^v represented as 2g by 2g integer matrices, along with their g by g tangent representation
 }
     pairs, hToL := GeometricHomomorphismRepresentation(Omega, DualLatticeWithRespectToPairing(Omega, E), RationalsExtra(Precision(BaseRing(Omega))));
     skewZZ := [* elt : elt in pairs | elt[2] + Transpose(elt[2]) eq 0 *];
@@ -30,6 +38,9 @@ end intrinsic;
 
 intrinsic RationalPolarizationBasis(Omega::ModMatFldElt, E::AlgMatElt) -> SeqEnum
 {
+    Input: Omega, 2g by 2g complex matrix representing a complex torus A, together with an (alternating) 2g by 2g integer matrix E irepresenting a polarization A to A^v (defined over Q)
+
+    Output: A basis of self-dual homomorphisms from A -> A^v defined over Q, given as 2g by 2g integer matrices, along with their g by g tangent representation
 }
     basis, hToL := SelfDualHomomorphisms(Omega, E);
     L := Codomain(hToL);
@@ -42,7 +53,9 @@ end intrinsic;
 
 // To show up at some point in Endomorpisms package
 intrinsic AdjugateMatrix(M::AlgMatElt) -> AlgMatElt
-{The adjugate matrix, i.e., Adj(A)*A = det(A) = A*Adj(A)}
+{
+    The adjugate matrix, i.e., Adj(A)*A = det(A) = A*Adj(A)
+}
     return Matrix(BaseRing(M), [[Cofactor(M, j, i) : j in [1..Ncols(M)]] : i in [1..Nrows(M)]]);
 end intrinsic;
 
@@ -77,25 +90,6 @@ function PfaffianAndMinors(Es, P)
 end function;
 
 
-function _IsPolarization(coord, pf, minors : Principal:=false)
-// Tests whether or not &+[ coord[i]*Es[i] : i in [1..n] ] defines a polarization on P.
-
-// check determinant
-det := Evaluate(pf, coord)^2;
-if det eq 0 or (Principal and det ne 1) then
-    return false;
-end if;
-// check positivity
-CC := BaseRing(Parent(minors[1]));
-for m in minors do
-    if Evaluate(m, coord) lt CC`epsinv then
-        return false;
-    end if;
-end for;
-
-return true;
-end function;
-
 
 intrinsic PrincipalPolarizations(Omega::ModMatFldElt, B::SeqEnum[AlgMatElt]) -> SeqEnum[AlgMatElt]
 {
@@ -104,11 +98,12 @@ intrinsic PrincipalPolarizations(Omega::ModMatFldElt, B::SeqEnum[AlgMatElt]) -> 
 }
     require #B eq 2: "Only implemented for combinations of two polarizations, as we rely on computing integral points on a conic in A^2";
     pf, minors := PfaffianAndMinors(B, Omega);
+    CC := BaseRing(Parent(minors[1]));
     // extract conic
     abc := [MonomialCoefficient(pf, [2-i,i]) : i in [0..2]];
     // extract combinations with determinant 1
     solutions := &cat [SetToSequence(elt) : elt in IntegralPointsConic(abc, [1,-1])];
     // check the positivity condition
-    ppols := [&+[ tup[i]*B[i] : i in [1..n] ] : tup in solutions | _IsPolarization(tup, pf, minors : Principal:=true)];
+    ppols := [&+[ coord[i]*B[i] : i in [1..n] ] : coord in solutions | forall{Evaluate(m, coord) lt CC`epsinv : m in minors}];
     return ppols;
 end intrinsic;
