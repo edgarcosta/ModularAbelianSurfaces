c297703cf85e3fc52d57f1087247cd9898bb8507
Edgar Costa <edgarc@mit.edu>
2023-08-08 17:31:10 -0400

    WriteStderr

--- abelian-varieties/dolabel.m
+++ abelian-varieties/dolabel.m
@@ -11,12 +11,16 @@ QQ := RationalsExtra(prec);
 
 Omega, E := PeriodMatrix(f : prec:=prec, Quotient:=false);
 CC := BaseRing(Omega);
-res_sub := ReconstructRationalGenus2Curves(Omega, E);
-
-Hquo_in_Hsub := Transpose(f`integral_homology_subquo[3]);
-Omega_quo, E_quo := PeriodMatrix(f : prec:=prec, Quotient:=true);
-res_quo := ReconstructRationalGenus2Curves(Omega_quo, E_quo);
-res_quo_from_sub := [* res_quo[1], [* <Hquo_in_Hsub*elt[1], elt[2]> : elt in res_quo[2] *], [* <Hquo_in_Hsub*elt[1], elt[2]> : elt in res_quo[3] *] *];
+try
+  res_sub := ReconstructRationalGenus2Curves(Omega, E);
+  Hquo_in_Hsub := Transpose(f`integral_homology_subquo[3]);
+  Omega_quo, E_quo := PeriodMatrix(f : prec:=prec, Quotient:=true);
+  res_quo := ReconstructRationalGenus2Curves(Omega_quo, E_quo);
+  res_quo_from_sub := [* res_quo[1], [* <Hquo_in_Hsub*elt[1], elt[2]> : elt in res_quo[2] *], [* <Hquo_in_Hsub*elt[1], elt[2]> : elt in res_quo[3] *] *];
+catch e
+  WriteStderr(e);
+  exit 1;
+end try;
 
 function TupleIsogCurve(elt)
     coeffs := elt[2] cmpeq false select [] else Eltseq(elt[2]);
@@ -28,4 +32,4 @@ end function;
 
 print StripWhiteSpace(Join([label] cat MachinePrintRes(res_sub) cat MachinePrintRes(res_quo), ":"));
 
-exit;
+exit 0;
